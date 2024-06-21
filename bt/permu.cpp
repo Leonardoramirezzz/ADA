66c5cf6dcbf41b8b9d4cb3be8b350a1d45ade973
Leonardoramirezzz <85765075+Leonardoramirezzz@users.noreply.github.com>
2024-06-21 04:50:07 -0500

    Create permu.cpp

---
+++ bt/permu.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> pesos = { 2, 3, 4, 5 };
+vector<int> beneficios = { 3, 4, 5, 6 };
+int capacidad = 5;
+
+int mejor_beneficio = 0;
+
+void permutaciones(vector<int>& indices, int inicio, int fin) {
+    if (inicio == fin) {
+        // Evaluamos la permutación actual
+        int beneficio_actual = 0;
+        int peso_actual = 0;
+        for (int i = 0; i < indices.size(); ++i) {
+            int index = indices[i];
+            if (peso_actual + pesos[index] <= capacidad) {
+                peso_actual += pesos[index];
+                beneficio_actual += beneficios[index];
+            }
+            else {
+                break; 
+            }
+        }
+
+        if (beneficio_actual > mejor_beneficio) {
+            mejor_beneficio = beneficio_actual;
+        }
+    }
+    else {
+        for (int i = inicio; i <= fin; ++i) {
+            swap(indices[inicio], indices[i]);
+            permutaciones(indices, inicio + 1, fin);
+            swap(indices[inicio], indices[i]);
+        }
+    }
+}
+
+int main() {
+    vector<int> indices(pesos.size());
+    for (int i = 0; i < pesos.size(); ++i) 
+        indices[i] = i;
+    
+
+    permutaciones(indices, 0, indices.size() - 1);
+    cout << "beneficio maximo: " << mejor_beneficio << endl;
+
+    return 0;
+}
